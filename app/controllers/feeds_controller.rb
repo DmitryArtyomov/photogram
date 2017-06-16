bd0c44cfe947ace48e476b10b2d92628ae542e1c
Dmitry Artemov <DmitryArtemov@coherentsolutions.com>
2017-06-16 11:02:36 +0300

    Modify loaded feed photos count

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -4,9 +4,9 @@ class FeedsController < ApplicationController
   def show
     @feed = current_user.feed_photos.order(created_at: :desc).includes(album: :user)
     if request.xhr? && params[:last]
-      render @feed.where("photos.id < #{params[:last]}").first(2)
+      render @feed.where("photos.id < #{params[:last].to_i}").first(5)
     else
-      @feed = @feed.first(2)
+      @feed = @feed.first(10)
     end
   end
 end
