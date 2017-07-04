a846fb4407c611e7a1161db77c49ad735319ecea
Dmitry Artemov <DmitryArtemov@coherentsolutions.com>
2017-07-04 14:25:57 +0300

    Implement middleware logger

---
+++ app/middleware/my_logger.rb
@@ -0,0 +1,22 @@
+class MyLogger
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    request_started_on = Time.now
+    @status, @headers, @response = @app.call(env)
+    request_ended_on = Time.now
+
+    write_to_log("Request took #{request_ended_on - request_started_on} seconds.")
+
+    [@status, @headers, @response]
+  end
+
+  private
+
+  def write_to_log(text)
+    log_text = "[#{Time.now.strftime('%F %T')}]: #{text}\n"
+    File.write('log/requests.log', log_text, mode: 'a')
+  end
+end
