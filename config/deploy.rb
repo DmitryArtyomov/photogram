3bc94308eb65dcb9542af36ae64dd61eeb581f8c
Dmitry Artemov <DmitryArtemov@coherentsolutions.com>
2017-07-13 13:27:30 +0300

    fixes

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :deploy_to, '/home/deployer/photogram'
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml", "config/application.yml"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
